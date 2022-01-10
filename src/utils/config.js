60cf8b63d9ead73749ed0bcbcd4601806f733fed
AmreshSinha <theamresh.sinha@gmail.com>
2022-01-10 15:11:58 +0530

    Add Config Utils

---
+++ src/utils/config.js
@@ -0,0 +1,7 @@
+export const Server = {
+    endpoint: process.env.ENDPOINT_URL,
+    project: process.env.PROJECT_ID,
+    usersCollection: process.env.USER_COLLECTION_ID,
+    professorsCollection: process.env.PROFESSORS_COLLECTION_ID,
+    coursesCollection: process.env.COURSES_COLLECTION_ID,
+};
