9ace620dc4428d34ae1133077a0d392d2c887135
AmreshSinha <theamresh.sinha@gmail.com>
2022-01-11 09:07:03 +0530

    Add Prof and Courses

--- src/api/api.js
+++ src/api/api.js
@@ -50,6 +50,10 @@ let api = {
         return api.provider().account.deleteSession("current");
     },
 
+
+    // Users Collection
+    // This collection contains other info about users like profile image, anonymous username
+
     createUser: (usersCollection, data, read, write) => {
         return api
           .provider()
@@ -66,10 +70,51 @@ let api = {
           .database.updateDocument(usersCollection, documentId, read, write);
     },
 
+    // No need to delete the User :kekw:
     // deleteUser: (usersCollection, documentId) => {
     //     return api.provider().database.deleteDocument(collectionId, documentId);
     // },
 
+
+    // Professors Collection
+    // This collection contains info about professors like their Department, Courses which he taught, Reviews, etc
+    
+    // We will add data from Dashboard or using a tool which I will write later
+    // createProf: (professorsCollection, data, read, write) => {
+    //     return api
+    //       .provider()
+    //       .database.createDocument(professorsCollection, data, read, write);
+    // },
+
+    listProf: (professorsCollection) => {
+        return api.provider().database.listDocuments(professorsCollection);
+    },
+
+    updateProf: (professorsCollection, documentId, read, write) => {
+        return api
+          .provider()
+          .database.updateDocument(professorsCollection, documentId, read, write);
+    },
+
+    // Courses Collection
+    // This collection contains info about courses, its department, professors, reviews, etc
+
+    // We will add data from Dashboard or using a tool which I will write later
+    // createProf: (coursesCollection, data, read, write) => {
+    //     return api
+    //       .provider()
+    //       .database.createDocument(coursesCollection, data, read, write);
+    // },
+
+    listProf: (coursesCollection) => {
+        return api.provider().database.listDocuments(coursesCollection);
+    },
+
+    updateProf: (coursesCollection, documentId, read, write) => {
+        return api
+          .provider()
+          .database.updateDocument(coursesCollection, documentId, read, write);
+    },
 };
 
 export default api;
