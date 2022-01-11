490d99f9b4a1d5c3c720da0ee0f9c99244955d5c
AmreshSinha <theamresh.sinha@gmail.com>
2022-01-11 13:18:27 +0530

    Add User

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -58,3 +58,53 @@ export const useGetProfReviews = (stale) => {
 
     return [stale];
 };
+
+export const useGetUser = () => {
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case FetchState.FETCH_INIT:
+                return { ...state, isLoading: true, isError: false };
+            case FetchState.FETCH_SUCCESS:
+                return {
+                    ...state,
+                    isLoading: false,
+                    isError: false,
+                    user: action.payload,
+                };
+            case FetchState.FETCH_FAILURE:
+                return { ...state, isLoading: false, isError: true };
+            default:
+                throw new Error();
+        }
+    };
+
+    const [state, dispatch] = useReducer(reducer, {
+        isLoading: false,
+        isError: true,
+        data: [],
+    });
+
+    useEffect(() => {
+        let didCancel = false;
+        const getUser = async () => {
+            dispatch({ type: FetchState.FETCH_INIT });
+            try {
+                const account = await api.getAccount();
+                if (!didCancel) {
+                    dispatch({
+                        type: FetchState.FETCH_SUCCESS,
+                        payload: account,
+                    });
+                }
+            } catch (e) {
+                if (!didCancel) {
+                    dispatch({ type: FetchState.FETCH_FAILURE });
+                }
+            }
+        };
+        getUser();
+        return () => (didCancel = true);
+    }, []);
+
+    return [state, dispatch];
+};
